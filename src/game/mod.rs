7b8ab5dfc4f124f940a3ca9cb2588ccb30ea4f19
aQaTL <mmsoltys@outlook.com>
2019-11-15 19:09:02 +0100

    Rewrite

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -1,30 +1,7 @@
-use std::time::Instant;
+pub struct Game {}
 
-pub struct GameClock {
-	pub start_time: Instant,
-	pub program_time: f32,
+impl Game {}
 
-	pub last_frame: Instant,
-	pub delta_time: f32,
-}
-
-impl GameClock {
-	pub(crate) fn update(&mut self) {
-		self.delta_time = self.last_frame.elapsed().as_micros() as f32 / 1_000_000.0;
-		self.last_frame = Instant::now();
-
-		self.program_time = self.start_time.elapsed().as_micros() as f32 / 1_000_000.0;
-	}
-}
-
-impl Default for GameClock {
-	fn default() -> Self {
-		GameClock {
-			start_time: Instant::now(),
-			program_time: 0.0,
-
-			last_frame: Instant::now(),
-			delta_time: 0.0,
-		}
-	}
+pub fn new() -> Game {
+	Game {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,221 +1,214 @@
-#![feature(const_fn)]
-#![deny(unsafe_code)]
-
-use std::path::Path;
-
-use crate::game::GameClock;
-use glium::backend::Facade;
-use glium::{
-	glutin::*,
-	texture::{RawImage2d, SrgbTexture2d},
-	uniforms,
+use gl::{types::*, *};
+use glutin::{Api, GlProfile, GlRequest};
+use std::os::raw::c_void;
+use std::str::from_utf8;
+use std::{
+	mem::{size_of, size_of_val},
+	ptr,
 };
-use imgui_winit_support::{HiDpiMode, WinitPlatform};
-use crate::ecs::{World, components};
-use crate::input::KeyState;
+use winit::{Event, WindowEvent};
 
-pub mod game;
-pub mod input;
-pub mod renderer;
-pub mod ecs;
+#[deny(unsafe_code)]
+mod game;
+
+static mut RUNNING: bool = true;
 
 fn main() {
-	println!("Size of SrgbTexture2d: {}", std::mem::size_of::<glium::texture::srgb_texture2d::SrgbTexture2d>());
-	println!("Size of TextureAny: {}", std::mem::size_of::<glium::texture::TextureAny>());
-	let mut events_loop = glium::glutin::EventsLoop::new();
-
-	let display = {
-		let window = glium::glutin::WindowBuilder::new()
-			.with_dimensions((800, 600).into())
-			.with_title(env!("CARGO_PKG_NAME"));
-
-		let context = glium::glutin::ContextBuilder::new()
-			.with_gl_profile(GlProfile::Core)
-			.with_vsync(false);
-
-		glium::Display::new(window, context, &events_loop).unwrap()
-	};
-
-	{
-		let window = display.gl_window();
-		let window = window.window();
-		window.hide_cursor(true);
-		if let Err(err) = window.grab_cursor(true) {
-			println!("Failed to grab the cursor: {}", err);
-		}
-	}
-
-	println!("{:?}", display.get_opengl_version());
-	println!("{:?}", display.get_opengl_vendor_string());
-	println!("{:?}", display.get_opengl_renderer_string());
-	println!();
-
-	let imgui = {
-		let mut ctx = imgui::Context::create();
-		ctx.set_ini_filename(None);
-
-		let mut platform = WinitPlatform::init(&mut ctx);
-		{
-			let gl_window = display.gl_window();
-			let window = gl_window.window();
-			platform.attach_window(ctx.io_mut(), &window, HiDpiMode::Rounded);
-		}
-
-		ctx
-			.fonts()
-			.add_font(&[imgui::FontSource::DefaultFontData {
-				config: Some(imgui::FontConfig {
-					size_pixels: 13.0,
-					..imgui::FontConfig::default()
-				}),
-			}]);
-
-		let renderer = imgui_glium_renderer::GliumRenderer::init(&mut ctx, &display).unwrap();
-
-		renderer::Imgui {
-			ctx,
-			platform,
-			renderer,
-		}
-	};
-
-	let input = {
-		let mut camera = input::Camera::new();
-		let window = display.gl_window();
-		input::toggle_mouse_grab(&mut camera, window.window());
-		input::Input {
-			camera: camera,
-			keys: input::Keys::new(),
-		}
-	};
-
-	let renderer = renderer::Renderer::new(display).unwrap();
-
-	let mut world = {
-		const INIT_CAPACITY: usize = 100;
-		World {
-			//TODO use with_capacity
-			entities: vec![components::NONE; INIT_CAPACITY],
-			positions: vec![[0.0, 0.0, 0.0].into(); INIT_CAPACITY],
-			sizes: vec![[0.0, 0.0, 0.0].into(); INIT_CAPACITY],
-			velocities: vec![Default::default(); INIT_CAPACITY],
-			sprites: vec![Default::default(); INIT_CAPACITY],
-			players: vec![Default::default(); INIT_CAPACITY],
-			jump_states: vec![Default::default(); INIT_CAPACITY],
-
-			clock: GameClock::default(),
-
-			textures: vec![],
-			input: input,
-			renderer: renderer,
-			imgui: imgui,
-		}
-	};
-	add_entities(&mut world);
-
-	let mut running = true;
-	while running {
-		world.update_clocks();
-
-//		if game.debug_mode {
-//			input::camera_process_movement_input(&mut camera, &keys, game.clock.delta_time);
-//		}
-//		input::camera_process_shortcuts(&display, &mut camera, &keys, game.clock.delta_time);
-
-		// Game update
-//		game.process_keyboard_input(&keys);
-//		game.update();
-
-		//Input handling
-		world.input.keys.update();
-		events_loop.poll_events(|event| {
-			match event {
-				Event::WindowEvent {
-					event: WindowEvent::CloseRequested,
-					..
-				} => {
-					running = false;
-				}
-				Event::DeviceEvent {
-					event: DeviceEvent::Key(key),
-					..
-				} => match key.virtual_keycode {
-					Some(key_code) => {
-						world.input.keys.0[key_code as usize] = match key.state {
-							ElementState::Released => KeyState::Released,
-							ElementState::Pressed => KeyState::JustPressed,
-						};
-					}
-					None => (),
-				},
-
-				_ => (),
-			}
-
-			world.handle_input_event(event);
-		});
-
-		world.update();
-
-		ecs::World::input(&mut world);
-		ecs::World::movement(&mut world);
-		ecs::World::render(&mut world);
-	}
+	unsafe { main_() }
 }
 
-fn handle_input() {
+unsafe fn main_() {
+	let mut event_loop = winit::EventsLoop::new();
 
+	let wb = winit::WindowBuilder::new().with_dimensions((1280, 720).into());
+
+	let window_ctx = glutin::ContextBuilder::new()
+		.with_gl_profile(GlProfile::Core)
+		.with_gl(GlRequest::Specific(Api::OpenGl, (4, 3)))
+		.with_vsync(true)
+		.build_windowed(wb, &event_loop)
+		.expect("Failed to initialise OpenGL")
+		.make_current()
+		.unwrap();
+
+	load_with(|s| window_ctx.get_proc_address(s) as *const _);
+	gl::Viewport::load_with(|s| window_ctx.get_proc_address(s) as *const _);
+
+	DebugMessageCallback(Some(debug_msg_callback), std::ptr::null_mut());
+
+	let vs = "#version 330 core
+layout (location = 0) in vec3 aPos;
+
+void main()
+{
+    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
+";
 
-//TODO texture storage
-fn add_entities(world: &mut World) {
-	use crate::ecs::components::*;
+	let fs = "#version 330 core
+out vec4 FragColor;
 
-	world.textures.push(
-		load_texture("images/box.png", &world.renderer.display).unwrap(),
+void main()
+{
+	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+}
+\0";
+
+	let shader = new_shader(vs, fs);
+
+	let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
+
+	let mut vao = 0u32;
+	GenVertexArrays(1, &mut vao as *mut _);
+	BindVertexArray(vao);
+
+	let mut vbo: u32 = 0;
+	GenBuffers(1, &mut vbo as *mut _);
+
+	BindBuffer(ARRAY_BUFFER, vbo);
+	BufferData(
+		ARRAY_BUFFER,
+		size_of_val(&vertices) as isize,
+		vertices.as_ptr() as *const _,
+		STATIC_DRAW,
 	);
 
-	//Player
-	let player = world.new_entity(Some(entity::PLAYER));
-	world.positions[player] = glm::Vec3::new(0.0, 0.0, 0.0);
-	world.sizes[player] = glm::Vec3::new(5.0, 5.0, 0.0);
-	world.sprites[player] = Sprite { tex_idx: 0 };
-	world.velocities[player] = Velocity {
-		velocity: glm::Vec3::new(0.0, 0.0, 0.0),
-		acceleration: [100.0, 40.0, 0.0].into(),
+	VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE, 12, ptr::null());
+	EnableVertexAttribArray(0);
+
+	BindBuffer(ARRAY_BUFFER, 0);
+	BindVertexArray(0);
+
+	Viewport(0, 0, 1280, 720);
+
+	while RUNNING {
+		event_loop.poll_events(|event: Event| match event {
+			Event::WindowEvent {
+				event: WindowEvent::CloseRequested,
+				..
+			} => RUNNING = false,
+			Event::WindowEvent {
+				event: WindowEvent::Resized(new_size),
+				..
+			} => {
+				let (width, height): (u32, u32) = new_size.into();
+				gl::Viewport(0, 0, width as i32, height as i32);
+			}
+			_ => {}
+		});
+
+		ClearColor(0.2, 0.3, 0.3, 1.0);
+		Clear(COLOR_BUFFER_BIT);
+
+		// Render
+		shader.bind();
+		BindVertexArray(vao);
+		DrawArrays(TRIANGLES, 0, 3);
+
+		if let Err(err) = window_ctx.swap_buffers() {
+			println!("swap_buffers err: {:?}", err);
+		}
+	}
+
+	drop(shader);
+	DeleteVertexArrays(1, &mut vao as *mut _);
+	DeleteBuffers(1, &mut vbo as *mut _);
+}
+
+extern "system" fn debug_msg_callback(
+	_source: u32,
+	_gltype: u32,
+	_id: u32,
+	severity: u32,
+	length: i32,
+	message: *const i8,
+	user_param: *mut c_void,
+) {
+	let (_param, msg) = unsafe {
+		let _param = &mut *(user_param as *mut ());
+		let msg = std::str::from_utf8_unchecked(std::slice::from_raw_parts::<u8>(
+			message as *const _,
+			length as usize,
+		));
+		(_param, msg)
 	};
-	world.players[player] = Player {
-		state: JumpState::Standing,
-		max_jump_count: 2,
+	println!("[DEBUG ({})]: {}", severity, msg);
+}
+
+struct Shader(pub(crate) u32);
+
+impl Shader {
+	fn bind(&self) {
+		unsafe {
+			UseProgram(self.0);
+		}
+	}
+}
+
+unsafe fn new_shader(vertex_source: &str, fragment_source: &str) -> Shader {
+	let compile_shader = |source: &str, s_type| {
+		let shader = CreateShader(s_type);
+		ShaderSource(
+			shader,
+			1,
+			&(source.as_ptr() as *const i8) as *const _,
+			&(source.len() as i32),
+		);
+		CompileShader(shader);
+
+		let (mut success, mut info_log) = (false, [0u8; 512]);
+		GetShaderiv(shader, COMPILE_STATUS, &mut success as *mut _ as *mut _);
+		if !success {
+			GetShaderInfoLog(
+				shader,
+				512,
+				ptr::null_mut(),
+				info_log.as_mut_ptr() as *mut _,
+			);
+			panic!(format!(
+				"[Shader compile]: Failed to compile {} shader: {}",
+				match s_type {
+					VERTEX_SHADER => "vertex",
+					FRAGMENT_SHADER => "fragment",
+					_ => unimplemented!(),
+				},
+				from_utf8(&info_log).unwrap()
+			));
+		}
+		shader
 	};
-	world.jump_states[player] = JumpState::Standing;
+
+	let vertex_shader = compile_shader(vertex_source, VERTEX_SHADER);
+	let fragment_shader = compile_shader(fragment_source, FRAGMENT_SHADER);
+
+	let shader = CreateProgram();
+	AttachShader(shader, vertex_shader);
+	AttachShader(shader, fragment_shader);
+	LinkProgram(shader);
+
+	let (mut success, mut info_log) = (false, [0u8; 512]);
+	GetProgramiv(shader, LINK_STATUS, &mut success as *mut _ as *mut _);
+	if !success {
+		GetProgramInfoLog(
+			shader,
+			512,
+			ptr::null_mut(),
+			info_log.as_mut_ptr() as *mut _,
+		);
+		println!(
+			"ERROR::SHADER::PROGRAM::LINKING_FAILED: {}",
+			from_utf8(&info_log).unwrap()
+		);
+	}
+
+	DeleteShader(vertex_shader);
+	DeleteShader(fragment_shader);
+
+	Shader(shader)
 }
 
-fn load_texture<P: AsRef<Path>, F: Facade>(
-	path: P,
-	facade: &F,
-) -> Result<SampledSrgbTexture2d, glium::texture::TextureCreationError> {
-	let image = image::open(path).unwrap().to_rgba();
-	let image_dimensions = image.dimensions();
-	let image = RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
-	let texture = SrgbTexture2d::new(facade, image)?;
-	Ok(SampledSrgbTexture2d {
-		tex: texture,
-		sampler_behavior: uniforms::SamplerBehavior {
-			minify_filter: uniforms::MinifySamplerFilter::Nearest,
-			magnify_filter: uniforms::MagnifySamplerFilter::Nearest,
-			..Default::default()
-		},
-	})
-}
-
-pub struct SampledSrgbTexture2d {
-	pub tex: SrgbTexture2d,
-	pub sampler_behavior: uniforms::SamplerBehavior,
-}
-
-impl uniforms::AsUniformValue for &SampledSrgbTexture2d {
-	fn as_uniform_value(&self) -> uniforms::UniformValue {
-		uniforms::UniformValue::SrgbTexture2d(&self.tex, Some(self.sampler_behavior))
+impl Drop for Shader {
+	fn drop(&mut self) {
+		unsafe { DeleteProgram(self.0) }
 	}
 }
